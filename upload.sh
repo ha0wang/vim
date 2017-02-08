d18f4d0547fc67ae3adf2d5791f9d89f4494b7f9
U-CORP\wangt23 <wangt23@CNENWANGT23L1C.corp.emc.com>
2017-02-08 10:26:06 +0800

    Wed, Feb 08, 2017 10:26:06 AM

--- upload.sh
+++ upload.sh
@@ -3,9 +3,9 @@
 #Copy config files to the current folder
 cp ~/.bashrc .
 cp ~/.vimrc .
-
+cp ~/.tmux.conf .
 #git add .bashrc
 #git add .vimrc
 
 git commit -am "`date`"
-git push https://ha0wang:git1o0%Cool!@github.com/ha0wang/vim.git
+git push https://ha0wang@github.com/ha0wang/vim.git
